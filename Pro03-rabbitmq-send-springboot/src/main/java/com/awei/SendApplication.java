21f9841209706f4a0bd769aaedf07f39c26a0375
jalivv <shizuwei99@gmail.com>
2022-01-09 14:10:01 +0800

    死信队列

--- Pro03-rabbitmq-send-springboot/src/main/java/com/awei/SendApplication.java
+++ Pro03-rabbitmq-send-springboot/src/main/java/com/awei/SendApplication.java
@@ -3,7 +3,6 @@ package com.awei;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableRabbit
@@ -11,9 +10,10 @@ public class SendApplication {
 
 
     public static void main(String[] args) {
-        ApplicationContext ac = SpringApplication.run(SendApplication.class, args);
+        SpringApplication.run(SendApplication.class, args);
+    }
+
+
 
 
     }
-
-}
